0e237cefda7d236f433c87ef6d3b85139b5068c2
Mégane Diéval <meganedieval@gmail.com>
2020-04-13 17:23:28 +0200

    last changes

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
 import occpollution
+import occpollution.map
+
+import webbrowser
+import os, sys
+
+
+occ_df = occpollution.Load_db().save_as_df()
+occpollution.map.plot_interactive_map(occ_df)
+
+
+webbrowser.open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "occpollution", "map", "map_occitanie.html"))
+webbrowser.open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "occpollution", "map", "map_occitanie_day1.html"))
