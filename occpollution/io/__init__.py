5bd7ba6b8a266d422e16a05e1c76c9d17858261b
Mégane Diéval <meganedieval@gmail.com>
2020-04-21 15:00:31 +0200

    last changes

--- occpollution/io/__init__.py
+++ occpollution/io/__init__.py
@@ -4,6 +4,9 @@ import os
 url_db = "https://opendata.arcgis.com/datasets/2ab16a5fb61f42c1a689fd9cc466383f_0.csv"
 # annual data for map
 url_db_an = "https://opendata.arcgis.com/datasets/e0f26f918c504608b10d128f65ce0efc_0.csv"
+# monthly data
+url_db_m = "https://opendata.arcgis.com/datasets/3acfa2aa5c0346a18ba7749c6885e503_0.csv"
 
 path_target = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "Mesure_journaliere.csv")
 path_target_an = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "Mesure_annuelle.csv")
+path_target_m = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "data", "Mesure_mensuelle.csv")
