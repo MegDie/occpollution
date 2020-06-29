d513ce438bd88e51e725212e8e8df492651f8497
elucsonjeanbptiste <63909372+elucsonjeanbptiste@users.noreply.github.com>
2020-06-29 08:35:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 from occpollution import __version__ as current_version
-
 setup(
   name='occpollution',
   version=current_version,
-  description='Visualization of Ozone pollution in Occitanie',
+  description='Visualization of ozone pollution in Occitania',  
   url='https://github.com/MegDie/occpollution',
-  author='Mégane Diéval, Gueladio Niasse, Jean-Baptiste Elucson',
+  author='MEGANE Diéval, GUELADIO Niasse, JEAN BAPTISTE Elucson',
   author_email='megane.dieval@etu.umontpellier.fr',
+  autor_email='gueladio.niasse@etu.umontpellier.fr',
+  author_email='jean-baptiste.elucson.etu.umontpellier.fr',
   license='MIT',
   packages=['occpollution', 'occpollution.map',
    'occpollution.ani', 'occpollution.preprocess', 'occpollution.io', 'occpollution.map'],
