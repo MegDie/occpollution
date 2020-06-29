19bc0d04b796a2a06c6cd81f1138eb29ed869cc8
elucsonjeanbptiste <63909372+elucsonjeanbptiste@users.noreply.github.com>
2020-06-29 08:45:13 -0700

    Update animation.py

--- animation.py
+++ animation.py
@@ -2,16 +2,15 @@ import occpollution
 from occpollution.io import path_target
 from occpollution.ani.animation import map_iteration
 from occpollution.ani.animation import animation
-
 import pandas as pd
-
 import webbrowser
-import os, sys
+import os 
+import sys
 
-# The following lines create a video of the ozone evolution in Occitanie during the last year
+# The following lines create a video of the ozone evolution in Occitania during the last year
 
-occ_j = pd.read_csv(path_target) # import data
-occ_j = occpollution.get_ozone_day(occ_j) # treatment of data
+occ_j = pd.read_csv(path_target) # Import of data
+occ_j = occpollution.get_ozone_day(occ_j) # Treatment of data
 map_iteration(occ_j)
 animation()
 
